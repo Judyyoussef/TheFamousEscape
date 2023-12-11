bae44445730f1de241eade99eac72590a8fdf102
Judy <judyhanyy@gmail.com>
2023-12-11 10:09:45 +0200

    Initial commit

---
+++ Assets/Bullet.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+
+    public float life = 3;
+    void Awake()
+    {
+        Destroy(gameObject, life);
+    }
+
+    // Update is called once per frame
+    void OnCollisionEnter(Collision collision)
+    {
+        Destroy(collision.gameObject);
+        Destroy(gameObject);
+    }
+}
